b8442e4cdc6f58d599161ae394ad4f05089729d0
PSDent <mnsan1004@gmail@com>
2017-10-20 17:23:52 +0900

    Commited

--- Search/DFS/2178_Searching Maze.cpp
+++ Search/DFS/2178_Searching Maze.cpp
@@ -1,16 +1,31 @@
-//===================================================
+//================================================
 // Filename : 2178_Seaching Maze
 //
 // Solved by PSDent.
 //
-// Problem URL : https://www.acmicpc.net/problem/2178
-//====================================================
+// https://www.acmicpc.net/problem/2178
+//================================================
 
 #include <iostream>
 
+void SearchMaze(char (*maze)[100], int, int);
+
 int main()
 {
+	char maze[100][100] = { '0' };
+	int N, M;
 
+	std::cin >> N >> M;
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < M; j++)
+			std::cin >> maze[i][j];
+
+	SearchMaze(maze, N, M);
 
 	return 0;
+}
+
+void SearchMaze(char (*maze)[100], int, int)
+{
+
 }
