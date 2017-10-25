58cc7fab06dbe0f95fdaeae47f4963e8a17cbe6b
PSDent <mnsan1004@gmail@com>
2017-10-25 20:14:52 +0900

    11403_Sarching Path

---
+++ Search/DFS/11403_Searching Path.cpp
@@ -0,0 +1,29 @@
+//================================================
+// Filename : 11403_Searching Path
+//
+// Solved by PSDent.
+// 
+// https://www.acmicpc.net/problem/11403
+//================================================
+#include <iostream>
+
+bool Matrix[100][100];
+bool AnwserMat[100][100];
+
+void DFS()
+{
+	while()
+}
+
+int main()
+{
+	int N;
+	
+	std::cin >> N;
+
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			std::cin >> Matrix[i][j];
+
+	return 0;
+}
