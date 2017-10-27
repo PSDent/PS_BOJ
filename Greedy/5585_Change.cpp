aec79bb6c2859ce44e6c36e0bc18a98004de5c85
PSDent <mnsan1004@gmail@com>
2017-10-27 10:32:55 +0900

    5585_Change

---
+++ Greedy/5585_Change.cpp
@@ -0,0 +1,39 @@
+//================================================
+// Filename : 5585_Change
+//
+// Solved by PSDent.
+// 
+// https://www.acmicpc.net/problem/5585
+//================================================
+#include <iostream>
+
+#define MONEY 1000
+int ChangeList[6] = { 500, 100, 50, 10, 5, 1 };
+int cnt = 0;
+
+void Greedy(int pay)
+{
+	int change = MONEY - pay;
+
+		for (int i = 0; i < 6; i++){
+			if (!change)
+				return;
+			cnt += change / ChangeList[i];
+			change = change - (change / ChangeList[i] * ChangeList[i]);
+		}
+
+	return;
+}
+
+int main()
+{
+	int pay;
+
+	std::cin >> pay;
+
+	Greedy(pay);
+
+	std::cout << cnt;
+
+	return 0;
+}
