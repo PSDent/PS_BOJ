4f9657f97961e74f7a5784753b8f97871a6b4dbc
PSDent <mnsan1004@gmail.com>
2017-11-14 23:51:32 +0900

    1932_NumTriangle.cpp

---
+++ DP/새 텍스트 문서.cpp
@@ -0,0 +1,53 @@
+//================================================
+// Filename : 1932_NumTriangle
+//
+// Solved by PSDent.
+//
+// https://www.acmicpc.net/problem/1932
+//================================================
+#include <iostream>	
+
+short N;
+long long DP[251001], arr[251001], max;
+
+void Bigger(long long a) {
+	max = max < a ? a : max;
+	return;
+}
+
+void Calculate()
+{
+	int index = 0;
+	DP[0] = arr[0];
+
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[index + i] + DP[index] > DP[index + i])
+				DP[index + i] = arr[index + i] + DP[index];
+
+			if (arr[index + i + 1] + DP[index] > DP[index + i + 1])
+				DP[index + i + 1] = arr[index + i + 1] + DP[index];
+			index++;
+		}
+	}
+
+	return;
+}
+
+int main()
+{
+	int t = 0;
+
+	std::cin >> N;
+	for (int i = 1; i <= N; i++) t += i;
+	for (int i = 0; i < t; i++) std::cin >> arr[i];
+	Calculate();
+
+	for (int i = t - N - 1; i < t; i++)
+		Bigger(DP[i]);
+
+	std::cout << max;
+	return 0;
+}
