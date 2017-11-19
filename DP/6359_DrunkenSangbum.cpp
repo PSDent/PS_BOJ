cdf55c7584aa667be15454d6a1bef2b1f6af5f7f
PSDent <mnsan1004@gmail.com>
2017-11-19 22:36:29 +0900

    6359

---
+++ DP/6359_DrunkenSangbum.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+bool room[101] = { false };
+int t, n, popul[10000];
+
+void Check(int index)
+{
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = i; j <= n; j += i)
+		{
+			if (i > 1)
+			{
+				if (room[j])
+					room[j] = false;
+				else
+					room[j] = true;
+			}
+			else
+				room[j] = true;
+		}
+	}
+
+	for (int i = 1; i <= n; i++)
+		if (room[i])
+			popul[index]++;
+
+	return;
+}
+
+int main()
+{
+	std::cin >> t;
+
+	for (int i = 0; i < t; i++)
+	{
+		std::cin >> n;
+		Check(i);
+	}
+
+	for (int j = 0; j < t; j++)
+		std::cout << popul[j] << std::endl;
+
+	return 0;
+}
