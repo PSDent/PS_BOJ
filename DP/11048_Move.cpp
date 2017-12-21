d5b11f3d43e4f260a75902a57a64c7300ae2f734
PSDent <mnsan1004@gmail@com>
2017-12-21 09:46:03 +0900

    11052

---
+++ DP/11048_Move.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int dp[1000][1000] = { 0 };
+int maze[1000][1000];
+int N, M;
+
+int MaxComp(int a, int b){
+	return a > b? a : b;
+}
+
+void CalMaxCandy()
+{
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (j > 0 && i > 0) dp[i][j] = MaxComp(dp[i - 1][j], dp[i][j - 1]) + maze[i][j];
+			else if (i == 0 && j > 0) dp[i][j] = dp[i][j - 1] + maze[i][j];
+			else if (j == 0 && i > 0) dp[i][j] = dp[i - 1][j] + maze[i][j];
+		}
+	}
+
+	return;
+}
+
+int main()
+{
+	std::cin >> N >> M;
+
+	for (int i = 0; i < N; i++) 
+		for (int j = 0; j < M; j++) {
+			std::cin >> maze[i][j];
+			dp[i][j] = maze[i][j];
+		}
+
+	CalMaxCandy();
+	std::cout << dp[N-1][M-1];
+
+	return 0;
+}
