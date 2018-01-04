2eefd7332ac44561742dff8c85277dc02115170d
PSDent <mnsan1004@gmail.com>
2018-01-04 16:20:49 +0900

    Create 11727_2xN Tiling.cpp

---
+++ DP/11727_2xN Tiling.cpp
@@ -0,0 +1,28 @@
+//================================================
+// Filename : 11727_2xN Tiling
+//
+// Solved by PSDent.
+//
+// https://www.acmicpc.net/problem/11727
+//================================================
+#include <iostream>
+
+int Tile(int n)
+{
+	int dp[1001] = { 0, 1, 3 };
+
+	for (int i = 3; i <= n; i++) 
+		dp[i] = (dp[i - 2] * 2 + dp[i - 1]) % 10007;
+
+	return dp[n];
+}
+
+int main()
+{
+	int N;
+
+	std::cin >> N;
+	std::cout << Tile(N);
+
+	return 0;
+}
