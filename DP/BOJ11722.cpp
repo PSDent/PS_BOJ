39db655d334f8e7d3fd032c2194c340214639d62
PSDent <mnsan1004@gmail.com>
2018-01-22 01:31:56 +0900

    Create BOJ11722.cpp

---
+++ DP/BOJ11722.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int dp[1001], arr[1001];
+
+int main()
+{
+	int N, max = 0;
+
+	std::cin >> N;
+	for (int i = 1; i <= N; i++) {
+		std::cin >> arr[i];
+
+		for (int j = 1; j < i; j++) {
+			if (arr[i] < arr[j] && dp[j] > dp[i])
+				dp[i] = dp[j];
+		}
+		dp[i] += 1;
+		if (max < dp[i])
+			max = dp[i];
+	}
+	std::cout << max;
+	return 0;
+}
