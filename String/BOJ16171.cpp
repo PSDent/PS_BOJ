7bea47ed32bd2880722ec4ff7ec8c01d31bec0e4
An Byeong Gwan <mnsan1004@gmail.com>
2018-10-05 23:55:05 +0900

    Update BOJ16171.cpp

--- String/BOJ16171.cpp
+++ String/BOJ16171.cpp
@@ -10,21 +10,31 @@ int main()
 
 	std::string book, keyword;
 	int index = 0;
+	int sIndex = 0;
 
 	std::cin >> book >> keyword;
 
+	int Cnt = 0;
 	for (int i = 0; i < book.size(); ++i)
 	{
-		if (book[i] == keyword[index]) ++index;
-		else if (book[i] >= '0' && book[i] <= '9') { continue;  }
-		else index = 0;
+		if (book[i] == keyword[index]) {
+			++index; ++Cnt;
+		}	
+		else if (book[i] >= '0' && book[i] <= '9') { continue; }
+		else
+		{
+			index = 0;
+			Cnt = 0;
+			if (book[i] == keyword[0]) --i;
+		}
 
-		if (index == keyword.size())
+		if (Cnt == keyword.size())
 		{
 			std::cout << 1;
 			return 0;
 		}
 	}
+	
 	std::cout << 0;
 	return 0;
 }
