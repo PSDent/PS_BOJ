aeb30b49331b9af115a15ae33fc27483d103fa08
An Byeong Gwan <mnsan1004@gmail.com>
2018-11-19 14:07:04 +0900

    Update BOJ16172.cpp

--- String/BOJ16172.cpp
+++ String/BOJ16172.cpp
@@ -1,40 +1,19 @@
 #include <iostream>
 #include <string>
-#include <ios>
 
 int main()
 {
-	std::ios::sync_with_stdio(false);
-	std::cin.tie(NULL);
-	std::cout.tie(NULL);
+	std::string word1, word2;
+	std::cin >> word1 >> word2;
 
-	std::string book, keyword;
-	int index = 0;
-	int sIndex = 0;
+	for (int i = 0; i < word1.size(); ++i)
+		if (word1[i] >= '0' && word1[i] <= '9')
+			word1.erase(i--, 1);
 
-	std::cin >> book >> keyword;
-
-	int Cnt = 0;
-	for (int i = 0; i < book.size(); ++i)
-	{
-		if (book[i] == keyword[index]) {
-			++index; ++Cnt;
-		}	
-		else if (book[i] >= '0' && book[i] <= '9') { continue; }
-		else
-		{
-			index = 0;
-			Cnt = 0;
-			if (book[i] == keyword[0]) --i;
-		}
-
-		if (Cnt == keyword.size())
-		{
-			std::cout << 1;
-			return 0;
-		}
-	}
-	
-	std::cout << 0;
+	int a = word1.find(word2);
+	if (a != std::string::npos)
+		std::cout << 1;
+	else
+		std::cout << 0;
 	return 0;
 }
