96cf6e5acb1efbb1315aa66a1aaa3cfc51d6d73d
An Byeong Gwan <mnsan1004@gmail.com>
2018-12-26 16:06:33 +0900

    Create BOJ2523.cpp

---
+++ Put the Stars/BOJ2523.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cout.tie(NULL);
+
+	int N;
+	std::cin >> N;
+
+	for (int i = 1; i <= N * 2 - 1; ++i)
+	{
+		int mid = N - abs(i - N);
+
+		for (int j = 0; j < mid; ++j)
+			std::cout << '*';
+		std::cout << '\n';
+	}
+
+	return 0;
+}
