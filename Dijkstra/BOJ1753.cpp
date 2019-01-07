464d163a284d4d1ffdd1560e4a84ad262a7b09d8
An Byeong Gwan <mnsan1004@gmail.com>
2019-01-08 08:13:56 +0900

    Update BOJ1753.cpp

--- Dijkstra/BOJ1753.cpp
+++ Dijkstra/BOJ1753.cpp
@@ -8,12 +8,15 @@ using Point = std::pair<int, int>;
 
 int V, E, K;
 int dest[20001];
-std::priority_queue<Point> pq;
+std::priority_queue<Point, std::vector<Point>, std::greater<Point>> pq;
 std::vector<Point> inV[20001];
-bool visit[20001];
 
 int main()
 {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+
 	int u, v, w;
 	std::cin >> V >> E >> K;
 
@@ -22,37 +25,36 @@ int main()
 	for (int i = 0; i < E; ++i)
 	{
 		std::cin >> u >> v >> w;
-		inV[u].push_back(std::make_pair(v, w));
+		inV[u].emplace_back(w, v);
 	}
 
 	dest[K] = 0;
-	pq.emplace(K, 0);
+	pq.emplace(0, K);
 	while (!pq.empty())
 	{
-		int srtIndex = pq.top().first;
-		int srtDist = pq.top().second;
+		int srtIndex = pq.top().second;
+		int srtDist = pq.top().first;
 		pq.pop();
+
+		if (dest[srtIndex] < srtDist) continue;
 
 		for (int i = 0; i < inV[srtIndex].size(); ++i)
 		{
-			int destIndex = inV[srtIndex][i].first;
-			int destDist = inV[srtIndex][i].second;
-			
+			int destIndex = inV[srtIndex][i].second;
+			int destDist = inV[srtIndex][i].first;
+
 			if (dest[destIndex] > srtDist + destDist)
 			{
 				dest[destIndex] = srtDist + destDist;
-				pq.emplace(destIndex, dest[destIndex]);
+				pq.emplace(dest[destIndex], destIndex);
 			}
 		}
-		visit[srtIndex] = true;
 	}
 
 	for (int i = 1; i <= V; ++i)
 	{
-		if (dest[i] == INF)
-			std::cout << "INF" << '\n';
-		else
-			std::cout << dest[i] << ' ' << '\n';
+		if (dest[i] == INF) std::cout << "INF" << '\n';
+		else std::cout << dest[i] << ' ' << '\n';
 	}
 
 	return 0;
